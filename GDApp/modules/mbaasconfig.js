1753b61bd390da405d43eeec4304e7377d5903b6
gdotter <gunndarr.dotter@kony.com>
2016-08-01 17:47:31 -0400

    New library and login form
    New library and login form

--- GDApp/modules/mbaasconfig.js
+++ GDApp/modules/mbaasconfig.js
@@ -1,9 +1,9 @@
 function getKonyMBAASAppKey()
 {
-	return "fc5d3d0aebaa83bfbe9e00134f85db85";
+	return $appKey;
 }
 
 function getKonyMBAASAppSecret()
 {
-	return "ea0dd0f3b69e0b425a63d142e3611bb2";
+	return $appSecret;
 }
